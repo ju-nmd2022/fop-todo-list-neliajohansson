7725fca3193e9a25273b7215227bb1c73dcc26ce
Nelia <cornelia.json@hotmail.com>
2023-04-06 13:38:13 +0200

    localstorage + display tasts/buttons

--- main.js
+++ main.js
@@ -23,3 +23,68 @@ window.addEventListener("load", () => {
 
   DisplayTasks();
 });
+
+function DisplayTasks() {
+  const taskList = document.querySelector("#task-list");
+  taskList.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const taskItem = document.createElement("div");
+    taskItem.classList.add("task-item");
+
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    const span = document.createElement("span");
+    const content = document.createElement("div");
+    const actions = document.createElement("div");
+    const deleteButton = document.createElement("button");
+
+    input.type = "checkbox";
+    input.checked = task.done;
+    span.classList.add("bubble");
+    if (task.category == "personal") {
+      span.classList.add("personal");
+    } else {
+      span.classList.add("school");
+    }
+
+    content.classList.add("task-content");
+    actions.classList.add("actions");
+    deleteButton.classList.add("delete");
+
+    content.innerHTML = `<input type="text" value="${task.content}" readonly>`;
+    deleteButton.innerHTML = "x";
+
+    label.appendChild(input);
+    label.appendChild(span);
+    actions.appendChild(deleteButton);
+    taskItem.appendChild(label);
+    taskItem.appendChild(content);
+    taskItem.appendChild(actions);
+
+    taskList.appendChild(taskItem);
+
+    if (task.done) {
+      taskItem.classList.add("done");
+    }
+
+    input.addEventListener("change", (e) => {
+      task.done = e.target.checked;
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+
+      if (task.done) {
+        taskItem.classList.add("done");
+      } else {
+        taskItem.classList.remove("done");
+      }
+
+      DisplayTasks();
+    });
+
+    deleteButton.addEventListener("click", (e) => {
+      tasks = tasks.filter((t) => t != task);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      DisplayTasks();
+    });
+  });
+}
